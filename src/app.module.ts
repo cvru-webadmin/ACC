3c28d09716551fc9e3f180afa9bd5e27e87190b8
Deepanshu-dashore <dashd9396@gmail.com>
2026-04-22 16:52:09 +0530

    feat: implement survey module with Prisma schema and CRUD boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { SurveyModule } from './survey/survey.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ConfigModule } from '@nestjs/config';
     PrismaModule,
     AuthModule,
     UserModule,
+    SurveyModule,
   ],
   controllers: [AppController],
   providers: [AppService],
